1dda08d733a550076beb0c8e9eb90e45f019d84d
PDochev <plamendochev0@gmail.com>
2024-12-06 21:10:20 +0000

    Change the class styles of the close button

--- components/modal.tsx
+++ components/modal.tsx
@@ -55,7 +55,10 @@ export default function Modal({ children }: { children: React.ReactNode }) {
         onClose={onDismiss}
       >
         {children}
-        <button onClick={onDismiss} className="close-button" />
+        <button
+          onClick={onDismiss}
+          className="absolute top-2 right-2 w-12 h-12 bg-transparent border-none rounded-2xl cursor-pointer flex items-center justify-center font-medium text-2xl after:content-['x'] after:text-black hover:bg-slate-100"
+        />
       </dialog>
     </div>,
     document.getElementById("modal-root")!
