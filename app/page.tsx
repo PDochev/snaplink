f9cc32c055a96b179b1c8840b5e23af5acff8e2f
PDochev <plamendochev0@gmail.com>
2024-12-15 21:37:21 +0000

    Adjust heading size in Home component for improved responsiveness and visual hierarchy

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
       <NavBar />
       <main className="mx-auto max-w-[1960px] mb-1">
         <div className="flex justify-center items-center">
-          <h1 className="scroll-m-20 text-2xl md:text-4xl font-extrabold tracking-tight lg:text-5xl mt-8 mb-8">
+          <h1 className="scroll-m-20 text-2xl sm:text-3xl md:text-4xl font-extrabold tracking-tight lg:text-5xl mt-8 mb-8">
             Welcome to SnapLink
           </h1>
         </div>
