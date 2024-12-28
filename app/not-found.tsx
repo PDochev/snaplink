e025f1001e000074ab593be504d706199fb26398
PDochev <plamendochev0@gmail.com>
2024-12-28 23:09:07 +0000

    Enhance NotFound component; add error logging and back navigation button for improved user experience

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
-export default function NotFound() {
+export default function NotFound({ error }: { error: Error }) {
+  const router = useRouter();
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-4">
       <h1 className="scroll-m-20 text-3xl md:text-4xl font-extrabold tracking-tight lg:text-5xl mt-8 mb-8 text-red-500">
         404
       </h1>
       <p>This page could not be found</p>
+      <Button
+        onClick={() => router.back()}
+        className="text-slate-400"
+        variant="link"
+      >
+        Back
+      </Button>
       <Button asChild>
         <Link href="/">Go back home</Link>
       </Button>
