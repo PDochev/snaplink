2c713bfa9ccfff4e2b0d1c61f645a3b6640087d9
PDochev <plamendochev0@gmail.com>
2025-04-01 15:08:19 +0100

    Refactor generateMetadata function to remove unnecessary Promise wrapper from params and improve description clarity

--- app/shared/[token]/layout.tsx
+++ app/shared/[token]/layout.tsx
@@ -1,19 +1,22 @@
 import type { Metadata } from "next";
 import { getSharedAlbum } from "@/app/lib/data";
 import { SharedAlbum } from "@/app/lib/definitions";
+
 export const generateMetadata = async ({
   params,
 }: {
-  params: Promise<{ token: string }>;
+  params: { token: string }; // Remove the Promise<> wrapper
 }): Promise<Metadata> => {
-  const token = (await params).token;
+  const token = params.token; // No need to await
   const album: SharedAlbum | null = await getSharedAlbum(token);
+
   if (!album) {
     return {
       title: "SnapLink | Shared Album",
-      description: "SnapLink is a photo sharing platform",
+      description: "SnapLink is a photo-sharing platform",
     };
   }
+
   return {
     title: `SnapLink | ${album.title}`,
     description: `Photo by ${album.user_name}`,
