3eb7966cadc1aee7fa36e0d20634e57a847d4e1a
PDochev <plamendochev0@gmail.com>
2025-06-26 21:28:44 +0100

    Refactor layout.tsx to comment out generateMetadata function and retain metadata generation using generateMetadataCustom

--- app/shared/[token]/layout.tsx
+++ app/shared/[token]/layout.tsx
@@ -1,26 +1,28 @@
 import type { Metadata } from "next";
 import { getSharedAlbum } from "@/app/lib/data";
 import { SharedAlbum } from "@/app/lib/definitions";
+import { generateMetadataCustom } from "@/app/utils/metadata";
 
-export const generateMetadata = async ({
-  params,
-}: {
-  params: Promise<{ token: string }>;
-  modal?: React.ReactNode; // Add this optional property
-}): Promise<Metadata> => {
-  const token = (await params).token;
-  const album: SharedAlbum | null = await getSharedAlbum(token);
-  if (!album) {
-    return {
-      title: "SnapLink | Shared Album",
-      description: "SnapLink is a photo sharing platform",
-    };
-  }
-  return {
-    title: `SnapLink | ${album.title}`,
-    description: `Photo by ${album.user_name}`,
-  };
-};
+// export const generateMetadata = async ({
+//   params,
+// }: {
+//   params: Promise<{ token: string }>;
+// }): Promise<Metadata> => {
+//   const token = (await params).token;
+//   const album: SharedAlbum | null = await getSharedAlbum(token);
+//   if (!album) {
+//     return {
+//       title: "SnapLink | Shared Album",
+//       description: "SnapLink is a photo sharing platform",
+//     };
+//   }
+//   return {
+//     title: `SnapLink | ${album.title}`,
+//     description: `Photo by ${album.user_name}`,
+//   };
+// };
+
+export const metadata: Metadata = generateMetadataCustom("Shared Album");
 
 export default function SharedAlbumsLayout({
   children,
